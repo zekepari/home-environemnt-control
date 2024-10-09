f49037dbf1781aa2d72e8076d492d6aef82bec4d
zekepari <pariezekiel@gmail.com>
2024-10-09 17:21:07 +1000

    revert

--- new.py
+++ new.py
@@ -1,15 +1,12 @@
-import os
 from gpiozero import DistanceSensor
 from time import sleep
-
-os.environ["GPIOZERO_PIN_FACTORY"] = "pigpio"
 
 sensor = DistanceSensor(echo=24, trigger=18)
 
 if __name__ == '__main__':
     try:
         while True:
-            dist = sensor.distance * 100  # Convert distance to cm
+            dist = sensor.distance * 100
             print(f"Measured Distance = {dist:.1f} cm")
             sleep(1)
 
